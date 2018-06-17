843de302379a0dfc5037573e60a0734061462ad3
Bradmkjr <brad@ucanbonline.net>
2018-06-17 15:41:23 -0600

    Forgot CORS function

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ var version = '2.0.1';
 // var legacy = express.Router();
 // var current = express.Router();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
