2d36c9c70e764b3286df97133f8846cab001d47b
Bradmkjr <brad@ucanbonline.net>
2018-06-16 19:11:45 -0600

    Added functions for common methods

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,46 @@ var faker = require("faker");
 
 faker.seed(42);
 
+function address(num){
+  faker.seed(parseInt(num));
+  var data = ({
+        ID: num,
+        adddressID: num,
+        streetAddress: faker.address.streetAddress(),
+        secondaryAddress: faker.address.secondaryAddress(),
+        city: faker.address.city(),
+        county: faker.address.county(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode(),
+        country: faker.address.country(),
+        phoneNumber: faker.phone.phoneNumber()
+      });
+  return data;
+}
+
+function user(num){
+  faker.seed(parseInt(num));
+  var addressID = faker.random.number();
+  var data = ({
+    ID: num,
+    userID: num,
+    prefix: faker.name.prefix(),      
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    suffix: faker.name.suffix(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    phoneNumber: faker.phone.phoneNumber(),
+    altPhoneNumber: faker.phone.phoneNumber(),
+    addressID: addressID,
+    address: address(addressID),
+    createdAt: faker.date.past(),
+    createdBy: faker.random.number(),
+  })
+  return data;
+}
+
 
 var appRouter = function (app) {
 
@@ -11,6 +51,7 @@ var appRouter = function (app) {
 
 /**
  * @api {get} /user/ Request A User information
+ * @apiVersion 1.0.1
  * @apiName GetUser
  * @apiGroup User
  *
@@ -19,11 +60,19 @@ var appRouter = function (app) {
  * @apiSuccess {String} lastname  Lastname of the User.
  */
   app.get("/user", function (req, res) {
+    var userID = faker.random.number();
+    var addressID = faker.random.number();
     var data = ({
+      ID: userID,
+      userID: userID,
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       username: faker.internet.userName(),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      addressID: addressID,
+      address: address(addressID),
+      createdAt: faker.date.past(),
+      createdBy: faker.random.number(),
     });
     res.status(200).send(data);
   });
@@ -41,14 +90,21 @@ var appRouter = function (app) {
  app.get("/users/:num", function (req, res) {
    var users = [];
    var num = req.params.num;
-
+   var addressID = faker.random.number();
    if (isFinite(num) && num  > 0 ) {
      for (i = 0; i <= num-1; i++) {
+       var userID = faker.random.number();
        users.push({
+           ID: userID,
+           userID: userID,
            firstName: faker.name.firstName(),
            lastName: faker.name.lastName(),
            username: faker.internet.userName(),
-           email: faker.internet.email()
+           email: faker.internet.email(),
+           addressID: addressID,
+           address: address(addressID),
+           createdAt: faker.date.past(),
+           createdBy: faker.random.number(),
         });
      }
 
@@ -76,6 +132,7 @@ var appRouter = function (app) {
 
    if (isFinite(num) && num  > 0 ) {
      faker.seed(parseInt(num));
+     var addressID = faker.random.number();
      user = ({
          ID: num,
          prefix: faker.name.prefix(),      
@@ -87,7 +144,8 @@ var appRouter = function (app) {
          password: faker.internet.password(),
          phoneNumber: faker.phone.phoneNumber(),
          altPhoneNumber: faker.phone.phoneNumber(),
-         addressID: faker.random.number(),
+         addressID: addressID,
+         address: address(addressID),
          createdAt: faker.date.past(),
          createdBy: faker.random.number(),
 
@@ -112,9 +170,17 @@ var appRouter = function (app) {
  * @apiSuccess {String} lastname  Lastname of the User.
  */
   app.get("/business", function (req, res) {
+    var businessID = faker.random.number();
+    var addressID = faker.random.number();
     var data = ({
+      ID: businessID,
+      businessID: businessID,
       name: faker.company.companyName(),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      addressID: addressID,
+      address: address(addressID),
+      createdAt: faker.date.past(),
+      createdBy: faker.random.number(),
     });
     res.status(200).send(data);
   });
@@ -134,15 +200,17 @@ var appRouter = function (app) {
 
     if (isFinite(num) && num  > 0 ) {
       faker.seed(parseInt(num));
+      var addressID = faker.random.number();
      var data = ({
       ID: num,
       name: faker.company.companyName(),
       email: faker.internet.email(),
       phoneNumber: faker.phone.phoneNumber(),
       faxNumber: faker.phone.phoneNumber(),
-      addressID: faker.random.number(),
+      addressID: addressID,
+      address: address(addressID),
       createdAt: faker.date.past(),
-      createdBy: faker.random.number(),   
+      createdBy: faker.random.number(),
       
     });
 
@@ -152,8 +220,6 @@ var appRouter = function (app) {
      res.status(400).send({ message: 'invalid ID supplied' });
    }
   });
-
-  
 
 /**
  * @api {get} /venue/:id Request Venue information
@@ -165,9 +231,17 @@ var appRouter = function (app) {
  * @apiSuccess {String} lastname  Lastname of the User.
  */
   app.get("/venue", function (req, res) {
+    var venueID = faker.random.number();
+    var addressID = faker.random.number();
     var data = ({
+      ID: venueID,
+      venueID: venueID,
       name: faker.company.companyName(),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      addressID: addressID,
+      address: address(addressID),
+      createdAt: faker.date.past(),
+      createdBy: faker.random.number(),
     });
     res.status(200).send(data);
   });
@@ -187,42 +261,24 @@ var appRouter = function (app) {
 
     if (isFinite(num) && num  > 0 ) {
       faker.seed(parseInt(num));
-     var data = ({
-      ID: num,
-      name: faker.company.companyName(),
-      email: faker.internet.email(),
-      phoneNumber: faker.phone.phoneNumber(),
-      faxNumber: faker.phone.phoneNumber(),
-      addressID: faker.random.number(),
-      createdAt: faker.date.past(),
-      createdBy: faker.random.number(),   
-      
-    });
-
-     res.status(200).send(data);
-    
+      var addressID = faker.random.number();
+      var data = ({
+        ID: num,
+        venueID: num,
+        name: faker.company.companyName(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.phoneNumber(),
+        faxNumber: faker.phone.phoneNumber(),
+        addressID: addressID,
+        address: address(addressID),
+        createdAt: faker.date.past(),
+        createdBy: faker.random.number(),      
+      });
+      res.status(200).send(data);
    } else {
      res.status(400).send({ message: 'invalid ID supplied' });
    }
   });
-
-
-// zipCode
-// city
-// cityPrefix
-// citySuffix
-// streetName
-// streetAddress
-// streetSuffix
-// streetPrefix
-// secondaryAddress
-// county
-// country
-// countryCode
-// state
-// stateAbbr
-// latitude
-// longitude
 
 /**
  * @api {get} /address/:id Request Address information
@@ -233,14 +289,8 @@ var appRouter = function (app) {
  * @apiSuccess {String} lastname  Lastname of the User.
  */
   app.get("/address", function (req, res) {
-    var data = ({
-      streetAddress: faker.address.streetAddress(),
-      secondaryAddress: faker.address.secondaryAddress(),
-      city: faker.address.city(),
-      state: faker.address.state(),
-      zipCode: faker.address.zipCode(),
-      phoneNumber: faker.phone.phoneNumber()
-    });
+    var addressID = faker.random.number();
+    var data = (address(addressID));
     res.status(200).send(data);
   });
 
@@ -257,19 +307,9 @@ var appRouter = function (app) {
   app.get("/address/:num", function (req, res) {
     var num = req.params.num;
     if (isFinite(num) && num  > 0 ) {
-     faker.seed(parseInt(num));
-     var data = ({
-      ID: num,
-      streetAddress: faker.address.streetAddress(),
-      secondaryAddress: faker.address.secondaryAddress(),
-      city: faker.address.city(),
-      state: faker.address.state(),
-      zipCode: faker.address.zipCode(),
-      createdAt: faker.date.past(),
-      createdBy: faker.random.number(),
-    });
-     res.status(200).send(data);
-    
+       faker.seed(parseInt(num));
+       var data = (address(num));
+       res.status(200).send(data);
    } else {
      res.status(400).send({ message: 'invalid ID supplied' });
    }
@@ -290,15 +330,14 @@ var appRouter = function (app) {
   app.get("/owner/:num", function (req, res) {
     var num = req.params.num;
     if (isFinite(num) && num  > 0 ) {
-     faker.seed(parseInt(num));
-     var data = ({
-      ID: num,
-      userID: faker.random.number(),
-      createdAt: faker.date.past(),
-      createdBy: faker.random.number(),
-    });
-     res.status(200).send(data);
-    
+      faker.seed(parseInt(num));
+      var data = ({
+        ID: num,
+        userID: faker.random.number(),
+        createdAt: faker.date.past(),
+        createdBy: faker.random.number(),
+      });
+     res.status(200).send(data);    
    } else {
      res.status(400).send({ message: 'invalid ID supplied' });
    }
@@ -401,28 +440,23 @@ var appRouter = function (app) {
     var num = req.params.num;
     if (isFinite(num) && num  > 0 ) {
      faker.seed(parseInt(num));
+     var employeeCount = faker.random.number() % 10;
+     var employees = [];
+     for (i = 0; i <= employeeCount; i++) {
+        var userID = faker.random.number();
+        employees.push({
+          ID: userID,
+          userID: userID,
+          user: user(userID),
+          createdAt: faker.date.past(),
+          createdBy: faker.random.number(),
+        });
+     }
      var data = ({
       ID: num,
-      userID: faker.random.number(),
-      createdAt: faker.date.past(),
-      createdBy: faker.random.number(),
-    });
-     res.status(200).send(data);
-    
-   } else {
-     res.status(400).send({ message: 'invalid ID supplied' });
-   }
-  });
-
-    app.get("/employed/:num", function (req, res) {
-    var num = req.params.num;
-    if (isFinite(num) && num  > 0 ) {
-     faker.seed(parseInt(num));
-     var data = ({
-      ID: num,
-      venueID: faker.random.number(),
-      createdAt: faker.date.past(),
-      createdBy: faker.random.number(),
+      venueID: num,
+      employees: employees,
+      
     });
      res.status(200).send(data);
     
@@ -443,15 +477,14 @@ var appRouter = function (app) {
  * @apiSuccess {String} lastname  Lastname of the User.
  */
 
-  app.get("/file/:num", function (req, res) {
+app.get("/employed/:num", function (req, res) {
     var num = req.params.num;
     if (isFinite(num) && num  > 0 ) {
      faker.seed(parseInt(num));
      var data = ({
       ID: num,
-      fileName: faker.system.commonFileName(),
-      fileType: faker.system.commonFileType(),
-      fileExtension: faker.system.commonFileExt(),
+      userID: num,
+      venueID: faker.random.number(),
       createdAt: faker.date.past(),
       createdBy: faker.random.number(),
     });
@@ -460,12 +493,44 @@ var appRouter = function (app) {
    } else {
      res.status(400).send({ message: 'invalid ID supplied' });
    }
-  });
+});
+
 
 /**
  * @api {get} /owns/:id Request Owner information
  * @apiName GetOwns
  * @apiGroup Ownership
+ *
+ * @apiParam {Number} id User unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+app.get("/file/:num", function (req, res) {
+  var num = req.params.num;
+  if (isFinite(num) && num  > 0 ) {
+   faker.seed(parseInt(num));
+   var data = ({
+    ID: num,
+    fileID: num,
+    fileName: faker.system.commonFileName(),
+    fileType: faker.system.commonFileType(),
+    fileExtension: faker.system.commonFileExt(),
+    createdAt: faker.date.past(),
+    createdBy: faker.random.number(),
+  });
+   res.status(200).send(data);
+  
+ } else {
+   res.status(400).send({ message: 'invalid ID supplied' });
+ }
+});
+
+/**
+ * @api {get} /owns/:id Request Owner information
+ * @apiName Logs
+ * @apiGroup Log
  *
  * @apiParam {Number} id User unique ID.
  *
@@ -480,30 +545,42 @@ var appRouter = function (app) {
      for (i = 0; i <= count; i++) {
        data.push({
         ID: num,
+        logID: num,
         createdAt: faker.date.past(),
         createdBy: faker.random.number(),
         idAddress: faker.internet.ip(),
         referrer: faker.internet.url(),
         userAgent: faker.internet.userAgent(),
-        action: faker.finance.transactionType(),
+        action: faker.hacker.ingverb(),
 
       });
      }
      res.status(200).send(data);    
   }); 
 
+/**
+ * @api {get} /log/:id Request recent log information
+ * @apiName LogsByID
+ * @apiGroup Log
+ *
+ * @apiParam {Number} id Log unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
   app.get("/log/:num", function (req, res) {
     var num = req.params.num;
     if (isFinite(num) && num  > 0 ) {
      faker.seed(parseInt(num));
      var data = ({
       ID: num,
+      logID: num,
       createdAt: faker.date.past(),
       createdBy: faker.random.number(),
       idAddress: faker.internet.ip(),
       referrer: faker.internet.url(),
       userAgent: faker.internet.userAgent(),
-      action: faker.finance.transactionType(),
+      action: faker.hacker.ingverb(),
     });
      res.status(200).send(data);
     
